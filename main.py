a866731a86e24d240a1d272aa570ba838229000d
Siajiza <>
2021-04-12 18:35:11 +0000

    Add second way to find out the solution

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 sales_prices = [
     100,
     83,
-    22,
+    220,
     40,
     100,
     400,
@@ -31,4 +31,43 @@ print(sales_prices_right)
 central_total_prices_sales = sorted_sales_prices[4]
 
 print(central_total_prices_sales)
+
+print('-----other way to know the final solution-----')
+
+# other way to know the final solution
+
+import math
+
+"""
+Tools:
+- math library
+- sorted function
+- list slicing
+- computations
+"""
+
+sale_prices1 = [
+  100,
+  83,
+  220,
+  40,
+  100,
+  400,
+  10,
+  1,
+  3
+]
+
+sorted_list = sorted(sale_prices1)
+num_of_sales = len(sorted_list)
+half_slice = math.floor(num_of_sales/2)
+first_sales_items = sorted_list[:half_slice]
+last_sales_items = sorted_list[-(half_slice):]
+median = sorted_list[half_slice:(half_slice + 1)]
+
+print(sorted_list)
+print(num_of_sales)
+print(first_sales_items)
+print(last_sales_items)
+print(median)
 
